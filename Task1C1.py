3185acea8bbd11e28a82d13af2a8ecfcd93a8a73
Mircea <bezneamirceaandrei21@gmail.com>
2024-05-27 21:35:45 +0300

    Am mai modularizat putin. In variables avem toate variabilele, fiecare cerinta o sa fie rezolvata intr-un fisier cod separat, sau cel putin asa sper

--- Task1C1.py
+++ Task1C1.py
@@ -1,69 +1,71 @@
 import csv
 from utils import det_list_type
 from utils import are_there_duplicates
+import variables
 
-PassengerId = []
-Survived = []
-Pclass = []
-Name = []
-Sex = []
-Age = []
-SibSp = []
-Parch = []
-Ticket = []
-Fare = []
-Cabin = []
-Embarked = []
+# PassengerId = []
+# Survived = []
+# Pclass = []
+# Name = []
+# Sex = []
+# Age = []
+# SibSp = []
+# Parch = []
+# Ticket = []
+# Fare = []
+# Cabin = []
+# Embarked = []
 with open("train.csv", 'r') as file:
 	reader = csv.reader(file, delimiter=",")
 	header = next(reader)
-	column_nr = len(header)
-	nr_lines = 0
+	variables.column_nr = len(header)
+	variables.nr_lines = 0
 	# Avem nr de coloane
 	for row in reader:
-		PassengerId.append(row[0])
-		Survived.append(row[1])
-		Pclass.append(row[2])
-		Name.append(row[3])
-		Sex.append(row[4])
-		Age.append(row[5])
-		SibSp.append(row[6])
-		Parch.append(row[7])
-		Ticket.append(row[8])
-		Fare.append(row[9])
-		Cabin.append(row[10])
-		Embarked.append(row[11])
-		nr_lines += 1
+		variables.PassengerId.append(row[0])
+		variables.Survived.append(row[1])
+		variables.Pclass.append(row[2])
+		variables.Name.append(row[3])
+		variables.Sex.append(row[4])
+		variables.Age.append(row[5])
+		variables.SibSp.append(row[6])
+		variables.Parch.append(row[7])
+		variables.Ticket.append(row[8])
+		variables.Fare.append(row[9])
+		variables.Cabin.append(row[10])
+		variables.Embarked.append(row[11])
+		variables.nr_lines += 1
 	# Avem numarul de linii
 
-	PassengerId_missing_count = len([item for item in PassengerId if item == ''])
-	Survived_missing_count = len([item for item in Survived if item == ''])
-	Pclass_missing_count = len([item for item in Pclass if item == ''])
-	Name_missing_count = len([item for item in Name if item == ''])
-	Sex_missing_count = len([item for item in Sex if item == ''])
-	Age_missing_count = len([item for item in Age if item == ''])
-	SibSp_missing_count = len([item for item in SibSp if item == ''])
-	Parch_missing_count = len([item for item in Parch if item == ''])
-	Ticket_missing_count = len([item for item in Ticket if item == ''])
-	Fare_missing_count = len([item for item in Fare if item == ''])
-	Cabin_missing_count = len([item for item in Cabin if item == ''])
-	Embarked_missing_count = len([item for item in Embarked if item == ''])
+	variables.PassengerId_missing_count = len([item for item in variables.PassengerId if item == ''])
+	variables.Survived_missing_count = len([item for item in variables.Survived if item == ''])
+	variables.Pclass_missing_count = len([item for item in variables.Pclass if item == ''])
+	variables.Name_missing_count = len([item for item in variables.Name if item == ''])
+	variables.Sex_missing_count = len([item for item in variables.Sex if item == ''])
+	variables.Age_missing_count = len([item for item in variables.Age if item == ''])
+	variables.SibSp_missing_count = len([item for item in variables.SibSp if item == ''])
+	variables.Parch_missing_count = len([item for item in variables.Parch if item == ''])
+	variables.Ticket_missing_count = len([item for item in variables.Ticket if item == ''])
+	variables.Fare_missing_count = len([item for item in variables.Fare if item == ''])
+	variables.Cabin_missing_count = len([item for item in variables.Cabin if item == ''])
+	variables.Embarked_missing_count = len([item for item in variables.Embarked if item == ''])
 	# Aflam cate elemente lipsesc din fiecare coloana
 
-	PassengerId_type = det_list_type(PassengerId)
-	Survived_type = det_list_type(Survived)
-	Pclass_type = det_list_type(Pclass)
-	Name_type = det_list_type(Name)
-	Sex_type = det_list_type(Sex)
-	Age_type = det_list_type(Age)
-	SibSp_type = det_list_type(SibSp)
-	Parch_type = det_list_type(Parch)
-	Ticket_type = det_list_type(Ticket)
-	Fare_type = det_list_type(Fare)
-	Cabin_type = det_list_type(Cabin)
-	Embarked_type = det_list_type(Embarked)
+	variables.PassengerId_type = det_list_type(variables.PassengerId)
+	variables.Survived_type = det_list_type(variables.Survived)
+	variables.Pclass_type = det_list_type(variables.Pclass)
+	variables.Name_type = det_list_type(variables.Name)
+	variables.Sex_type = det_list_type(variables.Sex)
+	variables.Age_type = det_list_type(variables.Age)
+	variables.SibSp_type = det_list_type(variables.SibSp)
+	variables.Parch_type = det_list_type(variables.Parch)
+	variables.Ticket_type = det_list_type(variables.Ticket)
+	variables.Fare_type = det_list_type(variables.Fare)
+	variables.Cabin_type = det_list_type(variables.Cabin)
+	variables.Embarked_type = det_list_type(variables.Embarked)
 	# Avem tipul listelor
 	
 	# Deoarece Id-ul pasagerului este unic, este suficient pentru a verifica
 	# daca exista duplicate sa vedem daca exista duplicate in lista id-urilor
-	found_duplicates = are_there_duplicates(PassengerId)
+	variables.found_duplicates = are_there_duplicates(variables.PassengerId)
+	
---
+++ Task1C2.py
@@ -0,0 +1,7 @@
+import variables
+import Task1C1
+# Se executa Task1 Pentru a avea setul de date dorit in variables.py
+# Orice variabila in variables este acum up to date si poate fi folosita
+# utilizand variables. inainte sa numim variabila
+
+print(variables.PassengerId)
