47427d701e4b27d0d7af0f6fd224af14768931f1
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-05-29 01:33:59 +0300

    added task1 cerinta 6

---
+++ Task1C6.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import variables as var
+import Task1C1
+import matplotlib.pyplot as plt
+import numpy as np
+
+very_young_people = var.data[var.data['Age'] <= 20]
+young_people = var.data[(var.data['Age'] > 20) & (var.data['Age'] <= 40)]
+middle_aged = var.data[(var.data['Age'] > 40) & (var.data['Age'] <= 60)]
+old_people = var.data[var.data['Age'] > 60]
+
+survived_very_young_people = very_young_people[very_young_people['Survived'] == 1]
+male_survived_very_young_people = survived_very_young_people[survived_very_young_people['Sex'] == 'male']
+
+survived_young_people = young_people[young_people['Survived'] == 1]
+male_survived_young_people = survived_young_people[survived_young_people['Sex'] == 'male']
+
+survived_middle_aged = middle_aged[middle_aged['Survived'] == 1]
+male_survived_middle_aged = survived_middle_aged[survived_middle_aged['Sex'] == 'male']
+
+survived_old_people = old_people[old_people['Survived'] == 1]
+male_survived_old_people = survived_old_people[survived_old_people['Sex'] == 'male']
+
+print("very young male survived: ", male_survived_very_young_people.shape[0])
+print("young male survived: ", male_survived_young_people.shape[0])
+print("middle aged male survived:", male_survived_middle_aged.shape[0])
+print("old male survived:", male_survived_old_people.shape[0])
+
+plt.bar('Very young male survived', male_survived_very_young_people.shape[0], label='Very young male survived')
+plt.bar('Young male survived', male_survived_young_people.shape[0], label='Young male survived')
+plt.bar('Middle Aged male survived', male_survived_middle_aged.shape[0], label='Middle Aged male survived')
+plt.bar('Old male survived', male_survived_old_people.shape[0], label='Old male survived')
+plt.show()
