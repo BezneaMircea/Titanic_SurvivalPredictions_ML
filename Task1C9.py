8febf8cf16fabd164adc577195af2d0683951474
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-05-30 10:54:31 +0300

    added task1 cerinta 9 file

---
+++ Task1C9.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import variables as var
+import Task1C1
+import matplotlib.pyplot as plt
+import numpy as np
+
+title = var.data['Name'].str.extract(r', ([^\.]+).')
+titles = title[0].value_counts()
+	
+
+plt.bar(titles.index, titles.values)
+plt.savefig("distributia_titlurilor_pe_sex.png")
+
